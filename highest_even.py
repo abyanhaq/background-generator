e597bbab23dbabc9773c9cc51bce55b750f35638
Abyan Haq <abyanuhaq@gmail.com>
2025-01-13 12:28:55 +0900

    new max_odd function

--- highest_even.py
+++ highest_even.py
@@ -5,6 +5,16 @@ def highest_even(li):
             cur_highest = i
     return(cur_highest)
 
+def highest_odd(li):
+    odd_list = []
+    for i in li:
+        if i%2 != 0:
+            odd_list.append(i)
+    return max(odd_list)
+            
+
+
 
 some_list = [10, 21, 3, 4, 9, 2, 22, 7, 12, 11]
 print(highest_even(some_list))
+print(highest_odd(some_list))
