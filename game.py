f6c8b91b488c61f184e4770bf6650dbbbda990c9
Abyan Haq <abyan.haq@ericsson.com>
2025-01-13 14:24:07 -0500

    added new files

---
+++ game.py
@@ -0,0 +1,10 @@
+class PlayerCharacter:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        print('run')
+
+player1 = PlayerCharacter('User')
+
+print(player1.run())
