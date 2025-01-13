0d5ce64748f3179c902d096ea3a9d9fe7180bf3f
Abyan Haq <abyanuhaq@gmail.com>
2025-01-13 11:55:35 +0900

    added files

---
+++ highest_even.py
@@ -0,0 +1,10 @@
+def highest_even(li):
+    cur_highest = 0
+    for i in li:
+        if i%2 == 0 and i > cur_highest:
+            cur_highest = i
+    return(cur_highest)
+
+
+some_list = [10, 21, 3, 4, 9, 2, 22, 7, 12]
+print(highest_even(some_list))
---
+++ password_manager.py
@@ -0,0 +1,4 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+print(f'{username} password, which is {password} is {len(password)} long')
